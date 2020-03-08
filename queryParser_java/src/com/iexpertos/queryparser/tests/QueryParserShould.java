b79f9dbb273303c712d8a2a2b50342bd9bbce99f
Ulrita <rhernandezmo@gmail.com>
2020-03-08 10:07:39 +0100

    Initial commit

---
+++ queryParser_java/src/com/iexpertos/queryparser/tests/QueryParserShould.java
@@ -0,0 +1,15 @@
+package com.iexpertos.queryparser.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.Test;
+
+class QueryParserShould {
+
+	@Test
+	void test() {
+		assertThat(true, is(true));
+	}
+
+}
